af404b071ccc35ebc803ef43c5b85ecb5121e962
samarthks <samarthk0105@gmail.com>
2022-01-19 13:45:53 -0500

    export to csv functionality

---
+++ exportcsv.js
@@ -0,0 +1,62 @@
+const pool = require("./database_setup");
+const fs = require("fs");
+const converter = require("json-2-csv");
+
+const exportAlldata = async (req, res) => {
+  try {
+    const getdata = await pool.query("SELECT * FROM PRODUCT_INVENTORY");
+
+    const jsondata = getdata.rows;
+    const todos = jsondata;
+
+    converter.json2csv(todos, (err, csv) => {
+      if (err) {
+        throw err;
+      }
+      //console.log(csv);
+      fs.writeFileSync("exportdata.csv", csv);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const exportparticulardata = async (req, res) => {
+  try {
+    const { product_id } = req.params;
+    const {
+      product_name,
+      date_of_order,
+      client_name,
+      client_email,
+      client_address,
+      quantity_ordered,
+    } = req.body;
+    let particulardata = {};
+    const getdata = await pool.query(
+      "SELECT * FROM PRODUCT_INVENTORY WHERE product_id=$1",
+      [product_id]
+    );
+    particulardata = getdata.rows[0];
+    if (!particulardata) {
+      particulardata = { Information: " No data Available " };
+    }
+    const todos = particulardata;
+
+    converter.json2csv(todos, (err, csv) => {
+      if (err) {
+        throw err;
+      }
+
+      //console.log(csv);
+      fs.writeFileSync("exportdata.csv", csv);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  exportAlldata,
+  exportparticulardata,
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const pool = require("./database_setup");
+
 const {
   create_product_data,
   updateparticulardata,
@@ -9,6 +9,7 @@ const {
   deleteAlldata,
   deleteparticulardata,
 } = require("./crud");
+const { exportAlldata, exportparticulardata } = require("./exportcsv");
 
 const app = express();
 const PORT = process.env.CLIENT_PORT;
@@ -16,7 +17,7 @@ const PORT = process.env.CLIENT_PORT;
 app.use(express.json());
 
 //insert data
-app.post("/product",create_product_data);
+app.post("/product", create_product_data);
 
 //Get all data
 app.get("/getproduct", getAlldata);
@@ -26,11 +27,18 @@ app.get("/getproduct/:product_id", getparticulardata);
 
 //edit data with particular id
 app.put("/updateproduct/:product_id", updateparticulardata);
+
 //delete data with particular id
 app.delete("/deleteproduct/:product_id", deleteparticulardata);
 
 //delete all data
 app.delete("/deleteproduct", deleteAlldata);
+
+//export all data to csv
+app.get("/exportproduct", exportAlldata);
+
+//export data with product id to csv
+app.get("/exportproduct/:product_id", exportparticulardata);
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
